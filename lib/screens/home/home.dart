bcb734e71888323887ac1f88bcafc7baf819a2cf
Joffrey Fortin <fortinjoffrey@gmail.com>
2020-04-28 16:02:00 +0200

    010 : Settings button, showModalBottomSheet
    - Home App Bar Settings Button
    - Opens a bottom sheet (closes when clicking outside)

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -10,6 +10,16 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    void _showSettingsPanel() {
+      showModalBottomSheet(
+          context: context,
+          builder: (context) {
+            return Container(
+                padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 60.0),
+                child: Text("Bottom sheet"));
+          });
+    }
+
     return StreamProvider<List<Brew>>.value(
       value: DatabaseService().brews,
       child: Scaffold(
@@ -26,6 +36,11 @@ class Home extends StatelessWidget {
               icon: Icon(Icons.person),
               label: Text("Logout"),
             ),
+            FlatButton.icon(
+              icon: Icon(Icons.settings),
+              label: Text("Settings"),
+              onPressed: () => _showSettingsPanel(),
+            )
           ],
         ),
         body: BrewList(),
